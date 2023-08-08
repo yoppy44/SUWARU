b8b4bc50c88d9d3b80105b27086944d5aabff78c
yoppy44 <136479890+yoppy44@users.noreply.github.com>
2023-08-08 16:28:40 +0900

    モデルのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true, length: { maximum: 10 }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は半角英数字で入力してください" } 
+
 end
