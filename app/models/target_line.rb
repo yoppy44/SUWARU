ca2fc377e5e3bc6ee2110841f3f3a8034f9e81e1
yoppy44 <136479890+yoppy44@users.noreply.github.com>
2023-08-09 13:08:26 +0900

    バリデーションの設定

---
+++ app/models/target_line.rb
@@ -0,0 +1,23 @@
+class TargetLine
+  include ActiveModel::Model 
+  attr_accessor :targets_name, :clothes, :age, :resemble, :user_id, :line_name, :station, :ride_time 
+    
+  with_options presence: true do
+    validates :targets_name
+    validates :clothes
+    validates :resemble
+    validates :user_id
+    validates :line_name
+    validates :station
+    validates :ride_time
+
+    validates :age, numericality: { other_than: 1 , message: "は未選択では登録できません"}
+    validates :line_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角で入力してください" }
+    validates :station, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角で入力してください" }
+
+  def save
+    target = Target.create(targets_name: targets_name, clothes: clothes, age: age, resemble: resemble, user_id: user_id)
+    Line.create(line_name: line_name, station: station, ride_time: ride_time, target_id: target.id)
+  end  
+  
+end  
