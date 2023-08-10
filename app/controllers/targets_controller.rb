1e6e81c150ec06f6562a44ad7acbb8b6bed149a0
yoppy44 <136479890+yoppy44@users.noreply.github.com>
2023-08-10 12:01:19 +0900

    モデルのバリデーション修正

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -5,21 +5,23 @@ class TargetsController < ApplicationController
   end
   
   def new
+    @target_line = TargetLine.new
   end  
 
   def create
-    @target = Target.create(target_params)
-    Line.create(line_params)
-    redirect_to root_path
+    @target_line = TargetLine.new(target_params)
+    if @target_line.valid?
+      @target_line.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end  
   end  
 
   private
 
   def target_params
-    params.permit(:targets_name, :clothes, :age, :resemble).merge(user_id: current_user.id)
+    params.require(:target_line).permit(:targets_name, :clothes, :age, :resemble, :line_name, :station, :ride_time).merge(user_id: current_user.id)
   end
-  
-  def line_params
-    params.permit(:line_name, :station, :ride_time).merge(target_id: @target.id)
-  end  
+ 
 end
--- app/models/target_line.rb
+++ app/models/target_line.rb
@@ -1,6 +1,6 @@
 class TargetLine
   include ActiveModel::Model 
-  attr_accessor :targets_name, :clothes, :age, :resemble, :user_id, :line_name, :station, :ride_time 
+  attr_accessor :targets_name, :clothes, :age, :resemble, :user_id, :line_name, :station, :ride_time, :target_id
     
   with_options presence: true do
     validates :targets_name
@@ -9,11 +9,11 @@ class TargetLine
     validates :user_id
     validates :line_name
     validates :station
-    validates :ride_time
-
+  end  
     validates :age, numericality: { other_than: 1 , message: "は未選択では登録できません"}
     validates :line_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角で入力してください" }
     validates :station, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角で入力してください" }
+    validates :ride_time, numericality: { other_than: 1 , message: "は未選択では登録できません"}
 
   def save
     target = Target.create(targets_name: targets_name, clothes: clothes, age: age, resemble: resemble, user_id: user_id)
